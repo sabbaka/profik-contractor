2f08f6f3e05f34e0fe5f121bb53666e8f42fa6d8
Kirill Glebov <sabbakaz@gmail.com>
2025-12-19 01:53:41 +0100

    fix: cleanup

--- components/maps/getMaps.native.ts
+++
@@ -1,9 +0,0 @@
-export function getMaps() {
-  // Native-only require to avoid web bundling
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const m: any = require('react-native-maps');
-  return { MapView: m.default || m.MapView, Marker: m.Marker } as {
-    MapView: any;
-    Marker: any;
-  };
-}
