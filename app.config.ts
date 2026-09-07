3ba47218f0bd8949f9fad2c6f0f84cffd97618ce
Kirill Glebov <sabbakaz@gmail.com>
2026-09-08 00:25:16 +0200

    chore(release): bump version to 1.0.13 (14)

--- app.config.ts
+++ app.config.ts
@@ -7,7 +7,7 @@ const LOCATION_WHEN_IN_USE_PERMISSION =
 const config = {
   name: 'profik-contractor',
   slug: 'profik-contractor',
-  version: '1.0.12',
+  version: '1.0.13',
   orientation: 'portrait',
   icon: './assets/images/icon.png',
   scheme: 'profikcontractor',
@@ -15,7 +15,7 @@ const config = {
   newArchEnabled: true,
   ios: {
     bundleIdentifier: 'com.profik.contractor',
-    buildNumber: '13',
+    buildNumber: '14',
     supportsTablet: false,
     infoPlist: {
       ITSAppUsesNonExemptEncryption: false,
@@ -25,7 +25,7 @@ const config = {
   },
   android: {
     package: 'com.profik.contractor',
-    versionCode: 13,
+    versionCode: 14,
     adaptiveIcon: {
       backgroundColor: '#E85D00',
       foregroundImage: './assets/images/android-icon-foreground.png',
